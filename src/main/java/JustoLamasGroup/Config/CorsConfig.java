fd5849254405694c072797caf1abe6de8b6ca2a1
lorenzo <lorenzothomasguido@gmail.com>
2025-12-11 20:33:06 -0300

    security update

--- src/main/java/JustoLamasGroup/Config/CorsConfig.java
+++ src/main/java/JustoLamasGroup/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",             // dev
+                                "https://jlgwebfront.vercel.app"     // ⭐ frontend en producción
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
